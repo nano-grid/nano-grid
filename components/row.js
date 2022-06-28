9570ae4248775b71f1bee803addde54b8a6b2559
Miguel <jemiguelrivas@gmail.com>
2022-06-28 11:28:17 -0400

    updating row web component

--- components/row.js
+++ components/row.js
@@ -1,43 +1,82 @@
-import { prepairClasses, validateProp, nano, checkAttr } from "../modules/helpers";
-import { validateSpacing } from "../modules/columns-manager";
-import { breakpointsType } from "../modules/types";
-
+import { nano } from "../modules/helpers";
+import { breakpointsType, spacingType } from "../modules/types";
 export default class Row extends HTMLElement {
   constructor() {
     super();
   }
 
-  connectedCallback() {
-    const group = checkAttr('group', this);
-    const vertical = checkAttr('vertical', this);
-    const grid = checkAttr('grid', this);
-    let role;
-    if (this.getAttribute('table-element')) {
-      role = 'row';
-      if (group) {
-        role = 'group';
+  toggleAttr() {
+    [...arguments].forEach(attr => {
+      if (this.hasAttribute(attr) && this.getAttribute(attr) !== 'false') {
+        this.classList.add(`${nano}${attr}`);
+      } else {
+        this.classList.remove(`${nano}${attr}`);
       }
+    })
+  }
+
+  updateBreakPoint() {
+    this.classList.remove(...breakpointsType);
+    const breakpointAttr = `${nano}break-${this.getAttribute('breakpoint')}`;
+    if (breakpointsType.includes(breakpointAttr)) {
+      this.classList.add(breakpointAttr);
     }
+  }
 
-    let breakpoint = validateProp('nn-row', 'breakpoint', this.getAttribute('breakpoint'), breakpointsType, '');
-    if (breakpoint === 'nn-') {
-      breakpoint = undefined;
+  updateSpacing() {
+    this.classList.remove(...spacingType);
+    const spacingAttr = `${nano}sp${+this.getAttribute('spacing') * 100}`;
+    if (spacingType.includes(spacingAttr)) {
+      this.classList.add(spacingAttr);
     }
+  }
 
-    const spacing = this.hasAttribute('spacing') ? validateSpacing(this.getAttribute('spacing')) : '';
+  updateRole() {
+    if (this.hasAttribute('table-element') && this.getAttribute('table-element') !== 'false') {
+      this.setAttribute('role', 'row');
+    } else if (this.hasAttribute('group') && this.getAttribute('group') !== 'false') {
+      this.setAttribute('role', 'group');
+    } else {
+      this.removeAttribute('role');
+    }
+  }
 
-    const classes = prepairClasses([
-      group,
-      breakpoint,
-      spacing,
-      vertical,
-      grid,
-      this.className || '',
-    ]);
-    this.className = classes;
+  updateClasses() {
+    this.updateRole();
+    this.toggleAttr('group', 'vertical', 'grid');
+    this.updateSpacing();
+    this.updateBreakPoint();
+  }
 
-    if (role) {
-      this.setAttribute('role', role);
+  connectedCallback() {
+    this.updateClasses();
+  }
+
+  static get observedAttributes() {
+    return ['group', 'breakpoint', 'spacing', 'vertical', 'grid', 'table-element'];
+  }
+
+  attributeChangedCallback(prop) {
+    switch (prop) {
+      case 'table-element':
+        this.updateRole();
+        break;
+      case 'group':
+        this.updateRole();
+        this.toggleAttr('group');
+        break;
+      case 'spacing':
+        this.updateSpacing();
+        break;
+      case 'vertical':
+        this.toggleAttr('vertical');
+        break;
+      case 'grid':
+        this.toggleAttr('grid');
+        break;
+      case 'breakpoint':
+        this.updateBreakPoint();
+        break;
     }
   }
 }
--- modules/types.js
+++ modules/types.js
@@ -1,8 +1,26 @@
 export const breakpointsType = [
-  "nn-",
-  "nn-sm",
-  "nn-md",
-  "nn-lg",
-  "nn-xl",
-  "nn-xll",
+  "nn-break-sm",
+  "nn-break-md",
+  "nn-break-lg",
+  "nn-break-xl",
+  "nn-break-xxl",
+];
+
+export const spacingType = [
+  "nn-sp25",
+  "nn-sp50",
+  "nn-sp75",
+  "nn-sp100",
+  "nn-sp125",
+  "nn-sp150",
+  "nn-sp175",
+  "nn-sp200",
+  "nn-sp225",
+  "nn-sp250",
+  "nn-sp275",
+  "nn-sp300",
+  "nn-sp325",
+  "nn-sp350",
+  "nn-sp375",
+  "nn-sp400",
 ];
