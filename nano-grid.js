d0c54a43e0df87c6d97f2de0bc63074a47a9c54d
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-05-28 10:20:33 -0700

    same fix

--- nano-grid.js
+++ nano-grid.js
@@ -1,3 +1,5 @@
+import nnCode from './js/nano_code.js'
+
 import './js/nano_btn.js'
 import './js/nano_caja.js'
 import './js/nano_desplazador.js'
@@ -7,6 +9,5 @@ import './js/nano_icono.js'
 import './js/nano_pilar.js'
 import './js/nano_pill.js'
 import './js/nano_video.js'
-import { nnCode } from './js/nano_spirit.js'
 
 export { nnCode }
