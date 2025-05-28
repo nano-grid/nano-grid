28de6405b0290a4fc69170ba8b5c7f062cb73b7a
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-05-28 10:18:24 -0700

    fixing export for nnCode

--- nano-grid.js
+++ nano-grid.js
@@ -1,10 +1,12 @@
-import "./js/nano_btn.js"
-import "./js/nano_caja.js"
-import "./js/nano_desplazador.js"
-import "./js/nano_fila.js"
-import "./js/nano_helpers.js"
-import "./js/nano_icono.js"
-import "./js/nano_pilar.js"
-import "./js/nano_pill.js"
-import "./js/nano_spirit.js"
-import "./js/nano_video.js"
+import './js/nano_btn.js'
+import './js/nano_caja.js'
+import './js/nano_desplazador.js'
+import './js/nano_fila.js'
+import './js/nano_helpers.js'
+import './js/nano_icono.js'
+import './js/nano_pilar.js'
+import './js/nano_pill.js'
+import './js/nano_video.js'
+import { nnCode } from './js/nano_spirit.js'
+
+export { nnCode }
